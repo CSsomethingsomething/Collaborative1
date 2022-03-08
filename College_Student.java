b737746d9cd021c616d05e806a0e0772797d3e9c
KenF12 <100365774+KenF12@users.noreply.github.com>
2022-03-08 00:14:33 -0500

    Update College_Student.java

--- College_Student.java
+++ College_Student.java
@@ -1,40 +1,38 @@
 public class College_Student extends Student{
-    // up to you ken if you wanna keep these public or make them private
-    
-    public String stu_id;
-    public double gpa;
+    private String major;
+    private double gpa;
 
     // probably constructors and accessors here
 
     public College_Student(){
         super();
-        this.stu_id = "";
+        this.major = "";
         this.gpa = 0.0;
     }
 
     
-    public College_Student(String n, int a, String s, boolean v, String s_i, double gp, String g, String stuid, double gpa){
+    public College_Student(String n, int a, String s, boolean v, String s_i, double gp, String g, String major, double gpa){
         super(n, a, s, v, s_i, gp, g);
-        this.stu_id = stuid;
+        this.major = major;
         this.gpa = gpa;
     }
 
     public College_Student(College_Student studentBase){
-        super(studentBase.getName(), studentBase.getAge(), studentBase.getSsn(), studentBase.getAlive(), studentBase.getStuId(), studentBase.getGPA(), studentBase.getGrade());
-        this.stu_id = studentBase.getStuId();
+        super(studentBase.getName(), studentBase.getAge(), studentBase.getSsn(), studentBase.getAlive(), studentBase.getStudentID(), studentBase.getGPA(), studentBase.getGrade());
+        this.major = studentBase.getMajor();
         this.gpa = studentBase.getGPA();
     }
 
-    public String getStuId(){
-        return this.stu_id;
+    public String getMajor(){
+        return this.major;
     }
 
     public double getGpa(){
         return this.gpa;
     }
 
-    public void setStuId(String arg){
-        this.stu_id = arg;
+    public void setMajor(String arg){
+        this.major = arg;
     }
 
     public void setGpa(double arg){
@@ -42,6 +40,9 @@ public class College_Student extends Student{
     }
 
     public String toString(){
-        return "Student id: " + this.stu_id + "\ngpa: " + this.gpa;
+        return "Student id: " + this.major + "\ngpa: " + this.gpa;
     }
 }
+
+
+
