5a1b734af79e76186796c7a8a656684ffbd53f0e
KenF12 <100365774+KenF12@users.noreply.github.com>
2022-03-08 10:28:10 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,69 +1,68 @@
-public class Person{
-    private String name;
-    private boolean alive;
-    private int age;
-    private String ssn;
+public class Person {
+	    private String name;
+	    private boolean alive;
+	    private int age;
+	    private String ssn;
 
-    public Person(){
-        this.name = "";
-        this.alive = true;
-        this.age = 0;
-        this.ssn = "";
-    }
+	    public Person(){
+	        this.name = "";
+	        this.alive = true;
+	        this.age = 0;
+	        this.ssn = "";
+	    }
 
-    public Person(String nameIn, boolean aliveIn, int ageIn, String ssnIn){
-        this.name = nameIn;
-        this.alive = aliveIn;
-        this.age = ageIn;
-        this.ssn = ssnIn;
-    }
+	    public Person(String nameIn, boolean aliveIn, int ageIn, String ssnIn){
+	        this.name = nameIn;
+	        this.alive = aliveIn;
+	        this.age = ageIn;
+	        this.ssn = ssnIn;
+	    }
 
-    public Person(Person person){
-        this.name = getName();
-        this.alive = getAlive();
-        this.age = getAge();
-        this.ssn = getSsn();
-    }
+	    public Person(Person person){
+	        this.name = getName();
+	        this.alive = getAlive();
+	        this.age = getAge();
+	        this.ssn = getSsn();
+	    }
 
-    // getters
-    public String getName(){
-        return this.name;
-    };
+	    // getters
+	    public String getName(){
+	        return this.name;
+	    };
 
-    public boolean getAlive(){
-        return this.alive;
-    };
+	    public boolean getAlive(){
+	        return this.alive;
+	    };
 
-    public int getAge(){
-        return this.age;
-    };
+	    public int getAge(){
+	        return this.age;
+	    };
 
-    public String getSsn(){
-        return this.ssn;
-    };
+	    public String getSsn(){
+	        return this.ssn;
+	    };
 
-    // setters
-    public void setName(String arg){
-        this.name = arg;
-    };
+	    // setters
+	    public void setName(String arg){
+	        this.name = arg;
+	    };
 
-    public void setAlive(boolean arg){
-        this.alive = arg;
-    };
+	    public void setAlive(boolean arg){
+	        this.alive = arg;
+	    };
 
-    public void setAge(int arg){
-        this.age = arg;
-    };
+	    public void setAge(int arg){
+	        this.age = arg;
+	    };
 
-    public void setSsn(String arg){
-        this.ssn = arg;
-    };
+	    public void setSsn(String arg){
+	        this.ssn = arg;
+	    };
 
-    public String toString(){
-        return "Person name: " + this.name + "\n" +
-        "Person is alive: " + this.alive + "\n" +
-        "Person age: " + this.age + "\n" +
-        "Person ssn: " + this.ssn;
-    }
-
+	    public String toString(){
+	        return "\nPerson name: " + this.name + "\n" +
+	        "Person is alive: " + this.alive + "\n" +
+	        "Person age: " + this.age + "\n" +
+	        "Person ssn: " + this.ssn + "\n\n";
+	    }
 }
