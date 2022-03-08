51df36a2557d2df212c17f8301aebbe0f11bc0c6
0xDEADBEAT <93951859+0xDEADBEAT@users.noreply.github.com>
2022-03-08 00:51:38 -0500

    department update

--- Department.java
+++ Department.java
@@ -1,98 +1,76 @@
 public class Department {
     //data
-	private String deptName;
-	private int numMajors;
-	private Teacher[] listTeachers; //inherits from Person class
-	private Student[] listStudents; //inherits from Person class
-	
-	/* Construct a department object (at least TWO constructors) */
-	//fully loaded constructor
-	public Department(String n, int m, Teacher[] t, Student[] s) {
-		
-		this.deptName = n;
-		this.numMajors = m;
-		
-		this.listTeachers = new Teacher[t.length];
-			for(int i = 0; i < t.length; i++) {
-				this.listTeachers[i] = new Teacher(t[i]);
-				}
-			
-		this.listStudents = new Student[s.length];
-			for(int j = 0; j < s.length; j++) {
-				this.listStudents[j] = new Student(s[j]);
-			}
-		
-		
-	}
+	private Professor chair;
+	private Professor[] faculty;
+	private Course[] courses;
 
-	/* Accessors and mutators (one pair per each feature) */
-	//set methods
-	public void setDepartmentName(String n) {
-		this.deptName = n;
-	}
-	
-	public void setNumberOfMajors(int m) {
-		this.numMajors = m;
-	}
-	
-	public void setListStudents(Student[] s) {
-		this.listStudents = new Student[s.length];
-		for(int j = 0; j < s.length; j++) {
-			this.listStudents[j] = new Student(s[j]);
+	public Department(){
+		this.chair = new Professor();
+		this.faculty = new Professor[]{};
+		this.courses = new Course[]{};
+	};
+	public Department(Department depin){
+
+		this.chair = new Professor(depin.getChair());
+		Professor[] facultyargin = depin.getFaculty();
+		this.faculty = new Professor[facultyargin.length];
+		for(int i = 0; i < facultyargin.length; i++){
+			this.faculty[i] = new Professor(facultyargin[i]);
 		}
-			
+		Course[] courseargin = depin.getCourses();
+		this.courses = new Course[courseargin.length];
+		for(int i = 0; i < courseargin.length; i++){
+			this.courses[i] = new Course(courseargin[i]);
+		}
+	};
+	public Department(Professor profin, Professor[] profsin, Course[] coursesin){
+		this.chair = new Professor(profin);
+
+		this.faculty = new Professor[profsin.length];
+		for(int i = 0; i < profsin.length; i++){
+			this.faculty[i] = new Professor(profsin[i]);
+		}
+		this.courses = new Course[coursesin.length];
+		for(int i = 0; i < coursesin.length; i++){
+			this.courses[i] = new Course(coursesin[i]);
+		}
+	};
+
+	public Professor getChair(){
+		return this.chair;
 	}
-	
-	public void setListTeachers(Teacher[] t) {
-		this.listTeachers = new Teacher[t.length];
-		for(int i = 0; i < t.length; i++) {
-			this.listTeachers[i] = new Teacher(t[i]);
-			}
+	public Professor[] getFaculty(){
+		return this.faculty;
 	}
-	
-	//get methods
-	public String getDepartmentName() {
-		return this.deptName;
-	}
-	
-	public int getNumberOfMajors() {
-		return this.numMajors;
-	}
-	
-	public Teacher[] getListTeachers() {
-		Teacher[] temp = new Teacher[this.listTeachers.length];
-			for(int p = 0; p < temp.length; p++) {
-				temp[p] = new Teacher(this.listTeachers[p]);
-			}
-		return temp;
-	}
-	
-	public Student[] getListStudents() {
-		Student[] temp = new Student[this.listStudents.length];
-			for(int k = 0; k < temp.length; k++) {
-				temp[k] = new Student(this.listStudents[k]);
-			}
-			
-		return temp;
+	public Course[] getCourses(){
+		return this.courses;
 	}
 
-	    /* toString method */
-	public String toString() {
-		String output = "";
-		
-		output += "Department Info:\n\nDepartment Name: " + this.deptName + "\n";
-		output += "\nNumber of Majors: " + this.numMajors + "\n";
-		output += "List of Teachers: ";
-			for(int q = 0; q < this.listTeachers.length; q++) {
-				output += "Teacher # " + (q + 1) + " Info: " + this.listTeachers[q].toString() + "\n\n";
-			}
-		output += "List of Students: ";
-			for(int m = 0; m < this.listStudents.length; m++) {
-				output += "Student # " + (m + 1) + " Info: " + this.listStudents[m].toString() + "\n\n"; 
-			}
-		
-		
-		
-		return output;
+	public void setChair(Professor argin){
+		this.chair = new Professor(argin);
+	}
+	public void setFaculty(Professor[] argin){
+		this.faculty = new Professor[argin.length];
+		for(int i = 0; i < argin.length; i++){
+			this.faculty[i] = new Professor(argin[i]);
+		}
+	}
+	public void setCourses(Course[] argin){
+		this.courses = new Course[argin.length];
+		for(int i = 0; i < argin.length; i++){
+			this.courses[i] = new Course(argin[i]);
+		}
+	}
+
+	public String toString(){
+		String retstring = "chair: " + this.chair.toString() + "\nfaculties:\n";
+		for(int i = 0; i < this.faculty.length; i++){
+			retstring += this.faculty[i].toString() + "\n";
+		}
+		retstring += "courses:\n";
+		for(int i = 0; i < this.courses.length; i++){
+			retstring += this.courses[i].toString() + "\n";
+		}
+		return retstring;
 	}
 }
