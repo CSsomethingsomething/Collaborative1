12b81f4ec0df9b413c7b10abf9df88f482cb2ffd
Pliang02 <100227200+Pliang02@users.noreply.github.com>
2022-03-08 20:09:19 -0500

    Fixed copy constructor

--- Course.java
+++ Course.java
@@ -1,90 +1,106 @@
 public class Course {
-	/*the basic features of a course*/
-	//data
-	public String courseName;
-	private String courseNumber;
-	public String instructorName;
-	private College_Student[] listCStudents;
-	
-	/*construct a course object (TWO constructors)*/
-	public Course(String course, String number, String instructor, College_Student[] cstudents) {
-		this.courseName = course;
-		this.courseNumber = number;
-		this.instructorName = instructor;
-		
-		this.listCStudents = new College_Student[cstudents.length];//allocate memory space
-			for(int i = 0; i < cstudents.length; i++) {
-				this.listCStudents[i] = new College_Student(cstudents[i]);
-			}
-		
-	}
-	//copy constructor
-	public Course(Course c) {
-		//error checking
-		if(c == null || c.courseName.equals("") || c.courseNumber.equals("") || c.instructorName.equals("") || c.listCStudents == null) {
-			System.out.println("Invalid course object! Stop!");
-			System.exit(0);
-		}
-		this.courseName = c.courseName;
-		this.courseNumber = c.courseNumber;
-		this.instructorName = c.instructorName;
-		this.listCStudents = c.getStudents();
-		
-	}
-	//set methods
-	public void setListStudents(College_Student[] cstudents) {
-		//this.students = students; NOT GOOD!!!!
-		this.listCStudents = new College_Student[cstudents.length];//request memory space
-		for(int i = 0; i < cstudents.length; i++) {
-			this.listCStudents[i] = new College_Student(cstudents[i]);
-		}
-	}
-	
-	public void setCourseName(String course) {
-		this.courseName = course;
-	}
-	
-	public void setCourseNumber(String number) {
-		this.courseNumber = number;
-	}
-	
-	public void setInstructorName(String instructor) {
-		this.instructorName = instructor;
-	}
-	
-	//get methods
-	public College_Student[] getStudents() {
-		College_Student[] temp = new College_Student[this.listCStudents.length];//request space in memory using internal data
-			for(int j = 0; j < temp.length; j++) {
-				temp[j] = new College_Student(this.listCStudents[j]);
-			}
-			return temp;	
-	}
-	
-	public String getCourseName() {
-		return this.courseName;
-	}
-	
-	public String getCourseNumber() {
-		return this.courseNumber;
-	}
-	
-	public String getInstructorNmae() {
-		return this.instructorName;
-	}
-	
-	//toString method
-	
-	public String toString() {
-		String output = "";
-		output += "\nCourse Info:\nCourse Name: " + this.courseName;
-		output += "\nCourse Number: " + this.courseNumber;
-		output += "\nInstructor Name: " + this.instructorName;
-		output += "\n\nStudent List: \n";
-				for(int k = 0; k < this.listCStudents.length; k++) {
-					output += "Student #" + (k + 1) + " Info:\n" + this.listCStudents[k].toString() + "\n";
-		}
-		
-		return output;
-	}
+    /*the basic features of a course*/
+    //data
+    public String courseName;
+
+    public Professor[] instructor;
+    private CollegeStudent[] listStudents;
+
+    /*construct a course object (TWO constructors)*/
+    public Course(String course, Professor[] inst, CollegeStudent[] students) {
+        this.courseName = course;
+
+        this.instructor = new Professor[inst.length];
+        for(int q = 0; q < inst.length; q++) {
+            this.instructor[q] = new Professor(inst[q]);
+        }
+
+        this.listStudents = new CollegeStudent[students.length];//allocate memory space
+        for(int i = 0; i < students.length; i++) {
+            this.listStudents[i] = new CollegeStudent(students[i]);
+        }
+
+    }
+    //copy constructor
+    public Course(Course c) {
+        //error checking
+        if(c == null || c.courseName.equals("") || c.listStudents == null) {
+            System.out.println("Invalid course object! Stop!");
+            System.exit(0);
+        }
+        this.courseName = c.courseName;
+
+        this.instructor = new Professor[c.instructor.length];
+        for(int q = 0; q < c.instructor.length; q++) {
+            this.instructor[q] = new Professor(c.instructor[q]);
+        }
+
+        this.listStudents = new CollegeStudent[c.listStudents.length];//allocate memory space
+        for(int i = 0; i < c.listStudents.length; i++) {
+            this.listStudents[i] = new CollegeStudent(c.listStudents[i]);
+        }
+
+    }
+    //set methods
+    public void setListStudents(CollegeStudent[] students) {
+        //this.students = students; NOT GOOD!!!!
+        this.listStudents = new CollegeStudent[students.length];//request memory space
+        for(int i = 0; i < students.length; i++) {
+            this.listStudents[i] = new CollegeStudent(students[i]);
+        }
+    }
+
+    public void setCourseName(String course) {
+        this.courseName = course;
+    }
+
+    public void setInstructor(Professor[] ins) {
+        this.instructor = new Professor[ins.length];
+        for(int q = 0; q < ins.length; q++) {
+            this.instructor[q] = new Professor(ins[q]);
+        }
+    }
+
+    //get methods
+    public CollegeStudent[] getStudents() {
+        CollegeStudent[] temp = new CollegeStudent[this.listStudents.length];//request space in memory using internal data
+        for(int j = 0; j < temp.length; j++) {
+            temp[j] = new CollegeStudent(this.listStudents[j]);
+        }
+        return temp;
+    }
+
+    public String getCourseName() {
+        return this.courseName;
+    }
+
+    public Professor[] getInstructor() {
+        Professor[] temp = new Professor[this.instructor.length];
+        for(int y = 0; y < temp.length; y++) {
+            temp[y] = new Professor(this.instructor[y]);
+        }
+        return temp;
+    }
+
+    //toString method
+
+    public String toString() {
+        String output = "";
+        output += "Course Info:\n\nCourse Name: " + this.courseName + "\n";
+
+        output += "\nInstructors List: ";
+        for(int h = 0; h < this.instructor.length; h++) {
+            output += "Instructor # " + (h + 1) + this.instructor[h].toString() + "\n\n";
+        }
+        output += "\nCollege Students List: ";
+        for(int k = 0; k < this.listStudents.length; k++) {
+            output += "Student # " + (k + 1) + " Info:\n" + this.listStudents[k].toString() + "\n";
+        }
+
+        return output;
+    }
+
+
+
+
 }
